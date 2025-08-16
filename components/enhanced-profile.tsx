7eb6cf017c299827ad2dfb80ca9b17c19ea80d4e
Abhijeet Srivastava <selvester69@gmail.com>
2025-08-17 01:05:00 +0530

    new fixes

--- components/enhanced-profile.tsx
+++ components/enhanced-profile.tsx
@@ -23,7 +23,7 @@ export function EnhancedProfile() {
           <div className="flex flex-col sm:flex-col items-center w-full">
             <div className="relative w-24 h-24 sm:w-28 sm:h-28 rounded-full overflow-hidden mb-4 border-2 border-cyan-400/20 ring-4 ring-zinc-800/50">
               <Image
-                src={personalInfo.avatar || "/placeholder.svg"}
+                src={personalInfo.avatar || "placeholder.svg"}
                 alt={personalInfo.name}
                 fill
                 className="object-cover"
